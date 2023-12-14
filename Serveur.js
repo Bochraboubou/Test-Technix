7354b68f4747a175dd882a57e5895cffe783e8a5
Bochraboubou <72579521+Bochraboubou@users.noreply.github.com>
2023-12-15 00:51:23 +0100

    Serveur Node.js
    Réponse pour la question : 
    Écrivez un serveur Node.js simple qui écoute sur le port 3000 et répond avec "Hello, World!" lorsqu'il est accédé. (fournissez la réponse sous la forme d'un lien GitHub)

---
+++ Serveur.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Le serveur est en cours d'écoute sur le port ${PORT}`);
+});
